4807d1300a2bde41cc099e34ebfa404fb5c4f605
Aathi Eswar S <aathi28082003@gmail.com>
2025-08-23 11:38:56 +0530

    char count with progress

--- app/components/chat/post-input.js
+++ app/components/chat/post-input.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
+import { computed } from "@ember/object";
 
 export default Component.extend({
+    maxCount: 40,
+    
+    inputCount: computed('inputValue', function() {
+        return this.inputValue ? this.inputValue.length : 0;
+    }),
 });
--- app/components/post.js
+++ app/components/post.js
@@ -1,13 +1,41 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
     isPostEditEnabled: true,
+    initialValue: '',
+    maxCharCount: 40,
+    charCount: 0,
+
+    didReceiveAttrs() {
+        this._super(...arguments);
+        this.set("initialValue", this.post.post.content);
+        this.set("charCount", this.post.post.content?.length || 0);
+    },
+
+    remaining: computed('charCount', 'maxCharCount', function () {
+        return this.maxCharCount - this.charCount;
+    }),
+
+    strokeOffset: computed('charCount', 'maxCharCount', function () {
+        let r = 18;
+        let circ = 2 * Math.PI * r;
+        let progress = Math.min(this.charCount / this.maxCharCount, 1);
+        return circ - progress * circ;
+    }),
+
     actions: {
         handleEdit(postId) {
-            this.toggleProperty("isPostEditEnabled")
+            this.toggleProperty("isPostEditEnabled");
             if (this.get("isPostEditEnabled")) {
-                this.onPostEdit(postId , this.post.post.content);
+                if (this.initialValue !== this.post.post.content) {
+                    this.onPostEdit(postId, this.post.post.content);
+                }
             }
+        },
+
+        handleInput(event) {
+            this.set("charCount", event.target.value.length);
         }
     }
 });
